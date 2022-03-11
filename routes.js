278493c17927b760a851f5c31e552668ed457750
Matheuslbrum <matheusdelimabrum@gmail.com>
2022-03-11 02:20:11 -0300

    creating routes

---
+++ routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const routes = express.Router()
+
+const materialsController = require('./src/users/index');
+
+routes.get('/rawMaterials', materialsController.consult)
+routes.post('/rawMaterials', materialsController.insert)
+routes.put('/rawMaterials/:id/request', materialsController.remove)
+
+
+
+module.exports = routes
