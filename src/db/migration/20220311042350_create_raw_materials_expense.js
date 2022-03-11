6f2b7c43aa8ecb970f0bf7d8771883cd8021a705
Matheuslbrum <matheusdelimabrum@gmail.com>
2022-03-11 03:12:58 -0300

    fix indentation issues in migration

--- src/db/migration/20220311042350_create_raw_materials_expense.js
+++ src/db/migration/20220311042350_create_raw_materials_expense.js
@@ -2,16 +2,16 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
- exports.up = knex => knex.schema.createTable('raw_materials_expense', (table) => {
-    table.increments("id").primary();
-    table.string("name").notNull();
-    table.integer("quantity").notNull();
-    table.string("user").notNull();
-    table.timestamp('created_date').defaultTo(knex.fn.now())
-  });
+exports.up = (knex) => knex.schema.createTable('raw_materials_expense', (table) => {
+  table.increments('id').primary();
+  table.string('name').notNull();
+  table.integer('quantity').notNull();
+  table.string('user').notNull();
+  table.timestamp('created_date').defaultTo(knex.fn.now());
+});
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
- exports.down = knex => knex.schema.dropTable('raw_materials_expense')
+exports.down = (knex) => knex.schema.dropTable('raw_materials_expense');
