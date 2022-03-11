37d1d4b3f0177be4cfdacb4f648de7d7a94dd516
Matheuslbrum <matheusdelimabrum@gmail.com>
2022-03-11 02:19:41 -0300

    adding middleware, json and fixing routes path

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const routes = require("./src/routes");
+const routes = require("./routes");
 
 const app = express ();
+app.use(express.json())
 
 app.use(routes);
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({error: error.message})
+});
 
 app.listen(3002, () => console.log('Server is running'))
