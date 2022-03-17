864e1c5b29674d2afaafd83e3423fa0c0916f44f
Matheuslbrum <matheusdelimabrum@gmail.com>
2022-03-17 01:39:21 -0300

    Editing columns names

--- src/db/migration/20220313064040_create_users_table.js
+++ src/db/migration/20220313064040_create_users_table.js
@@ -4,8 +4,8 @@
  */
 exports.up = (knex) => knex.schema.createTable('users', (table) => {
   table.increments('id').primary();
-  table.string('name').notNull();
-  table.string('user').notNull();
+  table.string('email').unique().notNull();
+  table.string('username').notNull();
   table.string('profession').notNull();
   table.timestamp('created_date').defaultTo(knex.fn.now());
 });
